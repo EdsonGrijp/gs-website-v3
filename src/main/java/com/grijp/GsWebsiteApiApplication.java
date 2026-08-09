daa2890ab3156a149947898f66df33a77c340e9d
Edson Grijp <edsongf95@hotmail.com>
2026-08-09 18:47:03 -0300

    feat: inicia projeto Spring Boot v3

---
+++ src/main/java/com/grijp/GsWebsiteApiApplication.java
@@ -0,0 +1,12 @@
+package com.grijp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsWebsiteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsWebsiteApiApplication.class, args);
+    }
+}
